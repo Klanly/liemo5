f6af0c7b3a72db03fa6b1ef95cd2c4dcaf6fc155
Li <75570317@qq.com>
2016-12-05 17:51:29 +0800

    new file

---
+++ Assets/Script/Manager/BundleManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+///1，这个类做bundle文件的计算
+public class BundleManager 
+{
+    private static BundleManager _instance = null; 
+    private int totalBytesToLoad=0;
+    private int totalBytesLoaded=0;
+
+    //文件名--版本号
+    private Dictionary<string,string> filenameDict = new Dictionary<string,string>();
+
+    private BundleManager()
+    {
+        System.IO.DirectoryInfo dirinfo = new DirectoryInfo(LMVersion.ASSET_BUNDLE_PATH);
+        if(dirinfo.Exists)
+        {
+            System.IO.FileInfo[] infos= dirinfo.GetFiles();
+            foreach(FileInfo fl in infos)
+            {
+                Debug.Log("获取到目录文件："+fl);
+
+
+            }
+        }
+    }
+    public static BundleManager getIns()
+    {
+        if(_instance == null)
+        {
+            _instance = new BundleManager();
+        }
+        return _instance;
+    }
+
+    public int TotalBytesToload(List<string> loadlist)
+    {
+        foreach(string ln in loadlist)
+        {
+            Debug.Log(ln);
+
+        }
+        return 1;
+    }
+}
---
+++ Assets/Script/Test/GetStringFromByte.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
+using System.Collections;
+
+public class GetStringFromByte : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        byte[] contents=File.ReadAllBytes(Application.streamingAssetsPath+"/Pc/ResTextList.bytes");
+        if(contents.Length>0){
+            Debug.Log("OK"+ contents.Length);
+            string str = System.Text.Encoding.Default.GetString ( contents );
+            Debug.Log(str);
+        }
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
