477356133a2eed28baabe621e05c46e2b2ca1b1a
Li <75570317@qq.com>
2016-12-08 14:23:35 +0800

    new up

---
+++ Assets/Script/Manager/CoroutineProxyManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CoroutineProxyManager : MonoBehaviour {
+    public delegate IEnumerator CoroutineMethod();
+    private static  CoroutineProxyManager _instance;
+    public static CoroutineProxyManager getIns(){
+        if(_instance==null){
+            Debug.LogError("模块没有初始化"); 
+        }
+        return _instance;
+    }
+
+    void Awake(){
+        _instance=this;
+        Debug.Log("Hello");
+    }
+
+    public void StartCoroutineDelegate(CoroutineMethod method){
+        StartCoroutine("RunCoroutine",method);
+    }
+
+    public IEnumerator RunCoroutine(CoroutineMethod method){
+        return method();
+    }
+}
